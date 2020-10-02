d2f329bd8d83dd4f828ea4e5d88f2c27926a402d
Dillon Porter <Dillonporter@hotmail.com>
2020-10-02 13:48:59 -0230

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+userForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const formData = new FormData(this);
+    const options = {
+        method: 'POST',
+        body: formData
+    };
+    
+    // Write the Fetch statement using https://httpbin.org/post along with the options. Then console log the JSON response. 
+    
+    fetch('https://httpbin.org/post', options)
+        .then(resp => resp.json())
+        .then(json => console.log(json.form))
+        .catch(err => console.error(err));
+    
+});
